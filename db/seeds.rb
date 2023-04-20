d40e6f7d1705ff8cae53e7378793587e385345df
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-20 15:44:12 -0500

    add initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+exercise = Exercise.create(name:"Lunges", directions: "Start by standing with your feet shoulder-width apart and arms down at your sides. Take a step forward with your right leg and bend your right knee as you do so, stopping when your thigh is parallel to the ground. Ensure that your right knee doesn’t extend past your right foot. Push up off your right foot and return to the starting position. Repeat with your left leg. This is one rep.", sets: 3, reps:15), 
+
+exercise = Exercise.create(name:"Push-ups", directions: "Start in a plank position. Your core should be tight, shoulders pulled down and back, and your neck neutral. Bend your elbows and begin to lower your body down to the floor. When your chest grazes it, extend your elbows and return to the start. Focus on keeping your elbows close to your body during the movement.", sets:3, reps:15), 
+
+exercise = Exercise.create(name:"Squats", directions: "Start by standing straight, with your feet slightly wider than shoulder-width apart, and your arms at your sides.Brace your core and, keeping your chest and chin up, push your hips back and bend your knees as if you’re going to sit in a chair.
+Ensuring your knees don’t bow inward or outward, drop down until your thighs are parallel to the ground, bringing your arms out in front of you in a comfortable position. Pause for 1 second, then extend your legs and return to the starting position.", sets: 2, reps: 15), 
+
+exercise = Exercise.create(name:"Dumbell Row", directions:"Start by standing straight, with your feet slightly wider than shoulder-width apart, and your arms at your sides.
+ Brace your core and, keeping your chest and chin up, push your hips back and bend your knees as if you’re going to sit in a chair. Ensuring your knees don’t bow inward or outward, drop down until your thighs are parallel to the ground, bringing your arms out in front of you in a comfortable position. Pause for 1 second, then extend your legs and return to the starting position.", sets:2, reps:15)
