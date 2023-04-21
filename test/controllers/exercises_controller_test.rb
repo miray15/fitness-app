c8551fefa7c68639a065ac8a286979ff058a2638
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-21 15:35:23 -0500

    add passed inde and create tests

--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -8,4 +8,11 @@ class ExercisesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Exercise.count, data.length
   end
+
+  test "create" do
+    assert_difference "Exercise.count", 1 do
+      post "/exercises.json", params: { name: "lat pulldown", video: "video", description: "go to the machine and pull", sets: 2, reps: 2 }
+      assert_response 200
+    end
+  end
 end
