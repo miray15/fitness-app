c5812451b5a9da40f31c50cc09b1f7cbbdc849ab
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-25 17:42:07 -0500

    add user_recipe model with user_id and recipe_id columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_212254) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_224012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_212254) do
     t.datetime "updated_at", null: false
     t.integer "exercise_id"
     t.integer "user_id"
+  end
+
+  create_table "user_recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
